8d8447673720ae74ab9f464e533f9991e9f19879
MuradHasan0 <workandstudy004@gmail.com>
2024-03-05 00:07:45 +0400

    US05 Class olusturuldu

---
+++ src/UserStorys/US05_SiparişListesiKontrolü.java
@@ -0,0 +1,22 @@
+package UserStorys;
+
+import Utility.Basedriver;
+import org.junit.Test;
+
+public class US05_SiparişListesiKontrolü extends Basedriver {
+
+    @Test
+    public void test(){
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
